16c1db0f5395dfd2107ff6ef0830776144043071
KhoaDauTay <cuonglieu3256@gmail.com>
2021-03-24 11:13:03 +0700

    Create Project

---
+++ CRUD/Controllers/SchoolsController.cs
@@ -0,0 +1,38 @@
+﻿using CRUD.Data_Access;
+using CRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD.Controllers
+{
+    public class SchoolsController : Controller
+    {
+        private SchoolContext _db;
+
+        public SchoolsController(SchoolContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            var course = _db.Courses.ToList();
+            return View(course);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            Course course = new Course();
+            return View(course);
+        }
+        [HttpPost]
+        public IActionResult Create(Course course)
+        {
+            _db.Courses.Add(course);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
